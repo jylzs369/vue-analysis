31da3bf3b63aa721f3f6e89ec1b9882a61a927bf
Ushio <jylzs369@163.com>
2018-12-19 14:44:25 +0800

    修改组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <router-view/>
+    <Hello :memberType="memberType" :dueId="dueId" />
   </div>
 </template>
 
 <script>
+import Hello from '@/components/Hello.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Hello
+  },
+  data () {
+    return {
+      memberType: 1,
+      dueId: 2
+    }
+  }
 }
 </script>
 
---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{ memberType }}{{ dueId }}</div>
+</template>
+
+<script>
+export default {
+    name: 'Hello',
+    props: {
+        memberType: Number,
+        dueId: Number
+    }
+}
+</script>
