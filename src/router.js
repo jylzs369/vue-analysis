74a3e887b3d6a808864e09b1a4202fbfb6275551
Ushio <jylzs369@163.com>
2018-12-19 14:43:45 +0800

    修改router错误

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Step1 from '@/components/Step1'
+// import Step1 from '@/components/Step1'
 
 Vue.use(Router)
 
+const routes = [
+]
+
 export default new Router({
+    routes
 })
