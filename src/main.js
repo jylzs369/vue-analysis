394e65406ab0188d6eb5f3df099dcf92e94014ca
Ushio <jylzs369@163.com>
2018-12-20 14:12:01 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import Axios from 'axios'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = Axios
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
